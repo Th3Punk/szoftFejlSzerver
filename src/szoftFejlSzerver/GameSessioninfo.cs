5297ae128969fecaf930d3a4f8705a24080779a2
Th3Punk <sitkei.balazs92@gmail.com>
2015-10-12 20:14:37 +0200

    kép visszaküldése
    Visszaküldi a kapozz elérésen található képet.

---
+++ src/szoftFejlSzerver/GameSessioninfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szoftFejlSzerver
+{
+    class Position
+    {
+        private int positionX { get; set; }
+        private int positionY { get; set; }
+    }
+    class GameSessioninfo
+    {
+    }
+}
